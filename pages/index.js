560d85cc17b296309b492fe972a7459d361d7f7a
xeroneon <andrewvasquez0113@gmail.com>
2019-12-10 13:49:13 -0700

    added nodemon to the project

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react"
+import PropTypes from "prop-types"
 
 function Index(props) {
-    return (
-        <>
-            <div>Hello</div>
-        </>
-    )
+	return (
+		<>
+			<div>{props.hostname}</div>
+		</>
+	)
 }
 
-export default Index;
+
+Index.getInitialProps = async ({ req }) => {
+	console.log("HEADERS", req.headers)
+	return { hostname: req.headers.host }
+}
+
+Index.propTypes = {
+	hostname: PropTypes.string
+}
+
+export default Index
