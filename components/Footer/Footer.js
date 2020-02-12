2cd3caf58dc90a52c9b4ae48fe693b5751ecd2ee
brionmodernsyntax <brion@modernsyntax.io>
2020-02-11 20:02:55 -0800

    styled the footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,17 +6,28 @@ function Footer() {
     return (
         <>
             <div className={styles.root}>
-                <img src="/images/pg-logo.png" />
-                <p className={styles.emails}>Get emails with all of the latest holistic medicine, preventative care news and media coverage</p>
-                <div className={styles.linkWrapper}>
-                    <p>About Us</p>
-                    <p>Terms of service</p>
-                    <p>Contact</p>
-                    <p>Privacy Policy</p>
+                <div className={styles.image}>
+                    <img src="/images/pg-logo.png" />
                 </div>
-                <div>
-                    <input type="email" placeholder="Email Address"/>
+                <div className={styles.email}>
+                    <p>Get emails with all of the latest holistic medicine, preventative care news and media coverage</p>
+                    <input type="email" placeholder="Email Address" />
                     <ActionButton>Join Our Newsletter</ActionButton>
+                </div>
+                <div className={styles.about}>
+                    <span className={styles.link}>About Us</span>
+                </div>
+                <div className={styles.terms}>
+                    <span className={styles.link}>Terms of service</span>
+                </div>
+                <div className={styles.contact}>
+                    <span className={styles.link}>Contact</span>
+                </div>
+                <div className={styles.privacy}>
+                    <span className={styles.link}>Privacy Policy</span>
+                </div>
+                <div className={styles.info}>
+                    <p>info@preventiongeneration.com - 1 (800) NEW-CARE (654-4389)</p>
                 </div>
             </div>
         </>
