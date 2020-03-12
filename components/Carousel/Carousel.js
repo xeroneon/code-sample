f06efd243636b6ac6a01720504446a594eb531a2
xeroneon <andrewvasquez0113@gmail.com>
2020-03-11 19:45:02 -0700

    i think i figured out horizontal scrolling

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,6 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Flickity from 'react-flickity-component';
-import normalizeWheel from 'normalize-wheel';
-import wheel from 'wheel';
 
 import styles from './Carousel.module.css'
 
@@ -10,61 +8,23 @@ const flickityOptions = {
     // initialIndex: 2,
     cellAlign: 'left',
     pageDots: false,
-    freeScroll: true
+    freeScroll: true,
+    contain: true
 }
 
+
 function Carousel(props) {
-
-    const slider = useRef(null)
-
-    useLayoutEffect(() => {
-        const flickity = slider.current.flkty;
-        let range = {
-            value: 0,
-            max: 800,
-            min: -800,
-            step: 1,
-            increase: function() {
-                const threshold = this.max / flickity.slides.length;
-                if(this.value < this.max) {
-                    this.value += this.step;
-                }
-                if(this.value >= threshold) {
-                    flickity.next();
-                    this.value -= threshold;
-                }
-            },
-            decrease: function() {
-                const threshold = this.max / flickity.slides.length;
-    
-                if(this.value > this.min) {
-                    this.value -= this.step;
-                }
-                if(this.value <= threshold) {
-                    flickity.previous();
-                    this.value += threshold;
-                }
-            }
-        };
-        wheel.addWheelListener(flickity.element, event => {
-            const wheelNormalized = normalizeWheel(event);
-            const direction = wheelNormalized.spinX * 100;
-            direction > 0 ? range.increase(direction) : range.decrease(direction);
-            flickity.startAnimation();
-        })
-    }, [slider])
     return (
         <>
             <div className={styles.headerBanner}> </div>
             <div className={styles.header}><h2>{props.header}</h2></div>
             <Flickity
-                className={styles.root} // default ''
+                className={`${styles.root} ${styles.scroll}`} // default ''
                 elementType={'div'} // default 'div'
                 options={flickityOptions} // takes flickity options {}
                 disableImagesLoaded={false} // default false
                 reloadOnUpdate // default false
                 static // default false
-                ref={slider}
             >
                 {props.children}
             </Flickity>
