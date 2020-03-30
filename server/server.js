cb3ccfe13d3a30c709bba9427f83b55eef494c42
xeroneon <andrewvasquez0113@gmail.com>
2020-03-29 20:24:17 -0700

    testing

--- server/server.js
+++ server/server.js
@@ -61,19 +61,19 @@ nextApp.prepare().then(() => {
         });
     });
 
-    // app.use(function(req, res, next) {
-    //     res.header('Access-Control-Allow-Credentials', true);
-    //     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    //     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    //     if ('OPTIONS' == req.method) {
-    //         res.send(200);
-    //     } else {
-    //         next();
-    //     }
-    // });
-
+    
     app.use(cors({credentials: true}));
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+        if ('OPTIONS' == req.method) {
+            res.send(200);
+        } else {
+            next();
+        }
+    });
     app.use(cookieParser());
     // app.options('/api/users', cors({credentials: true, origins: ['https://www.preventiongeneration.com', 'https://www.prevention-generation.herokuapp.com']}));
       
