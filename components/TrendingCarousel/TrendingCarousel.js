056b9983b301b7c2951f35661b185b943d32a338
xeroneon <andrewvasquez0113@gmail.com>
2020-04-09 21:33:20 -0700

    fixed same page article transitions

--- components/TrendingCarousel/TrendingCarousel.js
+++ components/TrendingCarousel/TrendingCarousel.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Carousel from 'components/Carousel/Carousel';
 import ArticleCard from 'components/ArticleCard/ArticleCard';
-// import WelcomeArticle from 'components/ArticleCard/WelcomeArticle';
-// import { UserContext } from 'contexts/UserProvider';
+import WelcomeArticle from 'components/ArticleCard/WelcomeArticle';
+import { UserContext } from 'contexts/UserProvider';
 
 function TrendingCarousel(props) {
-    // const { user } = useContext(UserContext);
+    const { user } = useContext(UserContext);
 
     return (
         <>
             <Carousel header={["Trending", <span key="sfdgnhdfgn"> Health </span>, <br key="an"/>, "posts" ]}>
-                {/* <WelcomeArticle key='welcome' /> */}
+                { !user && <WelcomeArticle key='welcome' /> }
                 {props.items.map(article => {
                     const authorName = [article.author.name, article.author.lastname].map(name => name.toLowerCase().replace(/\s/g, '_')).join('-');
                     return <ArticleCard 
