3f75c51e372792f00d1612c6560c80288875f529
xeroneon <andrewvasquez0113@gmail.com>
2020-04-16 20:17:45 -0700

    fixed carousel and subit content page

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,37 +1,34 @@
-import React, {  useRef } from 'react';
+import React, {  useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Carousel.module.css'
 
-
 function Carousel(props) {
     const carousel = useRef(null);
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const [ maxScroll, setMaxScroll ] = useState(9999);
 
     function scrollRight() {
         carousel.current.scrollLeft += 400;
+        setScrollPosition(carousel.current.scrollLeft + 400)
     }
     function scrollLeft() {
         carousel.current.scrollLeft -= 400;
+        setScrollPosition(carousel.current.scrollLeft - 400)
     }
+    useEffect(() => {
+        setMaxScroll(carousel.current.scrollWidth - carousel.current.clientWidth)
+    }, [carousel])
     return (
         <div className={styles.wrapper}>
             {/* <div className={styles.headerBanner}> </div> */}
             <div className={styles.header}><h2>{props.header}</h2></div>
             <div className={styles.arrowContainer}>
-                <div className={styles.arrow} onClick={scrollLeft}><i className="material-icons-outlined">chevron_left</i></div>
-                <div className={styles.arrow} onClick={scrollRight}><i className="material-icons-outlined">chevron_right</i></div>
+                <div className={`${styles.arrow} ${scrollPosition < 100 ? styles.disableArrow : null}`} onClick={scrollLeft}><i className="material-icons-outlined">chevron_left</i></div>
+                <div className={`${styles.arrow} ${scrollPosition > maxScroll ? styles.disableArrow : null}`} onClick={scrollRight}><i className="material-icons-outlined">chevron_right</i></div>
             </div>
-            {/* <Flickity
-                className={`${styles.root} ${styles.scroll}`} // default ''
-                elementType={'div'} // default 'div'
-                options={flickityOptions} // takes flickity options {}
-                disableImagesLoaded={false} // default false
-                reloadOnUpdate // default false
-                static // default false
-            > */}
             <div className={styles.root} ref={carousel}>
                 {props.children}
             </div>
-            {/* </Flickity> */}
         </div>
     )
 }
