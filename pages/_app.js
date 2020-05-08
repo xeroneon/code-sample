f07ed3a2df350e0e9763d925af32286eb53f2d8c
xeroneon <andrewvasquez0113@gmail.com>
2020-05-08 16:19:56 -0700

    limited image sizes

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ class MyApp extends App {
             <>
                 <Head>
                     {process.env.NODE_ENV !== 'production' && <meta name="robots" content="noindex" />}
-                    <title>Prevention Generation - Personalized Health Content</title>
+                    <title>Prevention Generation - Medically Reviewed Health Content + Social Media Personalization</title>
                     <meta property="og:description" content='the Prevention Generation is a new digital experience delivering personalized health and wellness content from holistic and conventional healthcare professionals.' />
                     <meta name="keywords" content="Prevention Generation" />
                     <link href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined" rel="stylesheet" />
